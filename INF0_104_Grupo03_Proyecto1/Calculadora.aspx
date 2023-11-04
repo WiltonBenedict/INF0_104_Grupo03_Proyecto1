49684dafa7c9163df5dccee864f052fdbe31d2e0
Wilton <wilton.benedict0354@uhispano.ac.cr>
2023-11-04 15:45:11 -0600

    Avance 11/4 (3:45PM)

--- INF0_104_Grupo03_Proyecto1/Calculadora.aspx
+++ INF0_104_Grupo03_Proyecto1/Calculadora.aspx
@@ -5,11 +5,171 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            font-family: "segoe UI";
+        }
+        .auto-style1 {
+            font-family: "segoe UI";
+            text-align: center;
+        }
+        .auto-style2 {
+            width: 100%;
+            border: 1px solid #666666;
+        }
+        .auto-style3 {
+            text-align: center;
+            height: 23px;
+        }
+        .auto-style7 {
+            width: 287px;
+            height: 23px;
+            text-align: center;
+        }
+        .auto-style10 {
+            width: 233px;
+            text-align: center;
+        }
+        .auto-style11 {
+            width: 233px;
+            height: 23px;
+            text-align: center;
+        }
+        .auto-style15 {
+            width: 259px;
+            text-align: center;
+        }
+        .auto-style16 {
+            width: 259px;
+            height: 23px;
+            text-align: center;
+        }
+        .auto-style17 {
+            width: 287px;
+            text-align: center;
+        }
+        .auto-style20 {
+            height: 23px;
+            width: 235px;
+            text-align: center;
+        }
+        .auto-style21 {
+            width: 235px;
+            text-align: center;
+        }
+        .auto-style22 {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+       <section>
+           <h1 class="auto-style1">Calculadora: Grupo 03</h1>
+           <h3 class="auto-style1">Curso: INFO-104. Proyecto 01.</h3>
+       </section>
+        <section>
+
+            <table class="auto-style2">
+                <tr>
+                    <td class="auto-style3" colspan="2">
+                        <asp:Label ID="tFormula" runat="server"></asp:Label>
+                    </td>
+                    <td class="auto-style7">
+                        <asp:Label ID="tConsola" runat="server"></asp:Label>
+                    </td>
+                    <td class="auto-style3" colspan="2">
+                        <asp:Label ID="tDato" runat="server"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style10">
+                        <asp:Button ID="bttRaiz" runat="server" OnClick="bttRaiz_Click" Text="√ " />
+                    </td>
+                    <td class="auto-style15">
+                        <asp:Button ID="bttBorrar" runat="server" OnClick="bttBorrar_Click" Text="C" />
+                    </td>
+                    <td class="auto-style17">
+                        <asp:Button ID="bttRemover" runat="server" OnClick="bttRemover_Click" Text="&lt;" />
+                    </td>
+                    <td class="auto-style21">
+                        <asp:Button ID="bttFactorial" runat="server" OnClick="bttFactorial_Click" Text="n!" />
+                    </td>
+                    <td class="auto-style22">
+                        <asp:Button ID="bttDiv" runat="server" OnClick="bttDiv_Click" Text="/" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style10">
+                        <asp:Button ID="bttElevarXY" runat="server" OnClick="bttElevarXY_Click" Text="xⁿ" />
+                    </td>
+                    <td class="auto-style15">
+                        <asp:Button ID="btt7" runat="server" OnClick="btt7_Click" Text="7" />
+                    </td>
+                    <td class="auto-style17">
+                        <asp:Button ID="btt8" runat="server" OnClick="btt8_Click" Text="8" />
+                    </td>
+                    <td class="auto-style21">
+                        <asp:Button ID="btt9" runat="server" OnClick="btt9_Click" Text="9" />
+                    </td>
+                    <td class="auto-style22">
+                        <asp:Button ID="bttMult" runat="server" OnClick="bttMult_Click" Text="*" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style11">
+                        <asp:Button ID="bttElevar10" runat="server" OnClick="bttElevar10_Click" style="height: 26px" Text="10ⁿ" />
+                    </td>
+                    <td class="auto-style16">
+                        <asp:Button ID="btt4" runat="server" OnClick="btt4_Click" Text="4" />
+                    </td>
+                    <td class="auto-style7">
+                        <asp:Button ID="btt5" runat="server" OnClick="btt5_Click" Text="5" />
+                    </td>
+                    <td class="auto-style20">
+                        <asp:Button ID="btt6" runat="server" OnClick="btt6_Click" Text="6" />
+                    </td>
+                    <td class="auto-style3">
+                        <asp:Button ID="bttRest" runat="server" OnClick="bttRest_Click" Text="-" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style10">
+                        <asp:Button ID="bttLog" runat="server" OnClick="bttLog_Click" Text="Log" />
+                    </td>
+                    <td class="auto-style15">
+                        <asp:Button ID="btt1" runat="server" OnClick="btt1_Click" Text="1" />
+                    </td>
+                    <td class="auto-style17">
+                        <asp:Button ID="btt2" runat="server" OnClick="btt2_Click" Text="2" />
+                    </td>
+                    <td class="auto-style21">
+                        <asp:Button ID="btt3" runat="server" OnClick="btt3_Click" Text="3" />
+                    </td>
+                    <td class="auto-style22">
+                        <asp:Button ID="bttSuma" runat="server" OnClick="bttSuma_Click" Text="+" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style11">
+                        <asp:Button ID="bttElevar2" runat="server" OnClick="bttElevar2_Click" Text="x²" />
+                    </td>
+                    <td class="auto-style16">
+                        <asp:Button ID="bttMasMenos" runat="server" OnClick="bttMasMenos_Click" Text="+/-" />
+                    </td>
+                    <td class="auto-style7">
+                        <asp:Button ID="btt0" runat="server" OnClick="btt0_Click" Text="0" />
+                    </td>
+                    <td class="auto-style20">
+                        <asp:Button ID="bttcomma" runat="server" OnClick="bttcomma_Click" Text="," />
+                    </td>
+                    <td class="auto-style3">
+                        <asp:Button ID="bttIgual" runat="server" OnClick="bttIgual_Click" Text="=" />
+                    </td>
+                </tr>
+            </table>
+
+        </section>
     </form>
 </body>
 </html>
--- INF0_104_Grupo03_Proyecto1/Calculadora.aspx.cs
+++ INF0_104_Grupo03_Proyecto1/Calculadora.aspx.cs
@@ -9,9 +9,235 @@ namespace INF0_104_Grupo03_Proyecto1
 {
     public partial class Calculadora : System.Web.UI.Page
     {
+        //INFO-104. Proyecto 01. Grupo 03.
+        //Instacia de la clase global
+        //Global global = new Global();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected void btt0_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "0";
+
+        }
+
+        protected void btt1_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "1";
+        }
+
+        protected void btt2_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "2";
+        }
+
+        protected void btt3_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "3";
+        }
+
+        protected void btt4_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "4";
+        }
+
+        protected void btt5_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "5";
+        }
+
+        protected void btt6_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "6";
+        }
+
+        protected void btt7_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "7";
+        }
+
+        protected void btt8_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "8";
+        }
+
+        protected void btt9_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            tDato.Text += "9";
+        }
+
+        protected void bttIgual_Click(object sender, EventArgs e)
+        {
+            //Ejemplo
+            ValidarControl();
+            if (Global.funcion == 1)
+            {
+                Global.val2 = Convert.ToDouble(tDato.Text);
+                Global.resultado = Global.val1 + Global.val2;
+                tFormula.Text = $"{Global.val1} + {Global.val2}";
+                tDato.Text = $"{Global.resultado}";
+                Global.funcion = 0; //Reinicia la funcion a cero para prevenir que entre en la estructura selectiva de igual.
+                Global.control = true; //Esto previene que se inicie una nueva operacion con numeros en la calculadora
+                //Si Global.control es true, presionar cualquier otra tecla causara que se borren los datos dentro de los textos para 
+                //iniciar una nueva operacion.
+            }
+            //Otras operaciones seguirian con else if
+        }
+        // OPERACIONES DE DOS VALORES
+        protected void bttSuma_Click(object sender, EventArgs e)
+        {
+            //NOTA: Operaciones de 2 valores Ejemplo
+            ValidarControl();
+            if (tDato.Text.Length > 0)
+            {
+                Global.funcion = 1;
+                //Guarda el valor del texto en val 1, limpia 
+                //el label tDato para introducir nuevos datos
+                //y asigna funcion 1 que indica a igual que
+                //la operacion es una suma
+                Global.val1 = Convert.ToDouble(tDato.Text);
+                tDato.Text = string.Empty;
+                tConsola.Text = "Suma";
+            }
+            else
+            {
+                //Mensaje de error en consola que no hay datos
+            }
+        }
+
+        protected void bttRest_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        protected void bttMult_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        protected void bttDiv_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        protected void bttElevarXY_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        //OPERACIONES DE UN VALOR
+        protected void bttLog_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        protected void bttElevar10_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        protected void bttRaiz_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        protected void bttFactorial_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        protected void bttElevar2_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+        }
+
+        //METODOS DE CONCATENACION
+        protected void bttMasMenos_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            if (tDato.Text.Contains('-'))
+            {
+                tDato.Text = tDato.Text.Substring(1);
+                //Se asume que - esta al principio del string y lo elimina
+            }
+            else
+            {
+                tDato.Text = "-" + tDato.Text;
+                //Agrega el menos al principio del string
+            }
+        }
+
+        protected void bttcomma_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            if (tDato.Text.Contains("."))
+            {
+                tConsola.Text = "Accion no posible";
+            }
+            else if (tDato.Text.Contains('-') && tDato.Text.Length == 1)
+            {
+                tConsola.Text = "Accion no posible";
+                //Excepcion para prevenir (-,)
+            }
+            else
+            {
+                tDato.Text = tDato.Text + ".";
+            }
+        }
+
+        protected void bttRemover_Click(object sender, EventArgs e)
+        {
+            ValidarControl();
+            if (tDato.Text.Length == 0)
+            {
+                tConsola.Text = "Nada que remover";
+            }
+            else
+            {
+                //Se remueve el ultimo caracter del string
+                tDato.Text = tDato.Text.Substring(0, tDato.Text.Length - 1);
+            }
+        }
+
+        protected void bttBorrar_Click(object sender, EventArgs e)
+        {
+            //Limpia todas las variables
+            ReiniciarVariables();
+        }
+        
+        private void ValidarControl()
+        {
+            if(Global.control == true)
+            {
+                //Se activa solo en caso una operacion ya fue completada.
+                ReiniciarVariables();
+            }
+        }
+
+        private void ReiniciarVariables()
+        {
+            tFormula.Text = string.Empty;
+            tDato.Text = string.Empty;
+            tConsola.Text = string.Empty;
+            Global.funcion = 0;
+            Global.val1 = 0;
+            Global.val2 = 0;
+            Global.resultado = 0;
+            Global.control = false;
+        }
+
+
     }
 }
